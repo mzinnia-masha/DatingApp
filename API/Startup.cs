2562fba2208471293af2d9f85b4e9f5683784e68
masha.dotnet@outlook.com <masha.dotnet@outlook.com>
2021-09-11 15:39:04 +0400

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,7 @@ namespace API
         {
 
             services.AddControllers();
+            services.AddCors();
             services.AddDbContext<DataContext>
                 (options =>
             {
@@ -52,6 +53,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
